7ea7bcc6a88239219374740ef90ce83f41ab0ce6
wilmaniklasson <wilma.niklasson2@hotmail.se>
2023-12-11 22:36:26 +0100

    css

--- src/dom.js
+++ src/dom.js
@@ -1,21 +1,21 @@
-
-
 function createSearchPokemonCard(pokemonDetailsArray) {
     const searchResults = document.querySelector('.search-results');
-    console.log(pokemonDetailsArray);
-    
+
     // Töm innehållet i behållaren innan du lägger till nya element
- searchResults.innerHTML = '';
+    searchResults.innerHTML = '';
+
+    // Skapa en ul-lista för att hålla Pokémon-korten
+    const resultList = document.createElement('ul');
+    resultList.classList.add('search-results-list');
 
     // Loopa igenom varje Pokemon i arrayen
     pokemonDetailsArray.forEach(pokemon => {
- 
-        const searchChampion = document.createElement('div');
+        const searchChampion = document.createElement('li');
         searchChampion.classList.add('search-champion');
 
         const imageDiv = document.createElement('div');
         imageDiv.classList.add('search-champion-img');
-      
+
         if (pokemon.image) {
             const image = document.createElement('img');
             image.src = pokemon.image;
@@ -27,16 +27,16 @@ function createSearchPokemonCard(pokemonDetailsArray) {
         const nameHeader = document.createElement('h2');
         nameHeader.textContent = pokemon.name;
 
-       
-
         const addToTeamBtn = document.createElement('button');
         addToTeamBtn.classList.add('add-to-team-btn');
         addToTeamBtn.textContent = 'Add to team';
 
-        
         addToTeamBtn.addEventListener('click', function() {
             // Lägg till den aktuella pokemonen i listan eller reservlistan
             addToTeam(pokemon);
+
+            // Uppdatera listorna efter att ha lagt till i team
+            updateLists();
         });
 
         infoDiv.appendChild(nameHeader);
@@ -45,26 +45,85 @@ function createSearchPokemonCard(pokemonDetailsArray) {
         searchChampion.appendChild(imageDiv);
         searchChampion.appendChild(infoDiv);
 
-     searchResults.appendChild(searchChampion);
+        resultList.appendChild(searchChampion);
     });
+
+    // Lägg till ul-listan i sökresultatbehållaren
+    searchResults.appendChild(resultList);
 }
+
 
 // arrayer för att lagra valda och reservpokemons
 const valdaPokemons = [];
 const reservPokemons = [];
 
-
 function addToTeam(pokemon) {
     if (valdaPokemons.length < 3) {
         valdaPokemons.push(pokemon);
+        createPokemonListElement('valdaPokemonsList', valdaPokemons);
     } else {
         reservPokemons.push(pokemon);
+        createPokemonListElement('reservPokemonsList', reservPokemons);
     }
-
-    console.log('Valda pokemons:', valdaPokemons);
-    console.log('Reservpokemons:', reservPokemons);
 }
 
+function createPokemonListElement(listId, pokemonList) {
+    const list = document.getElementById(listId);
+    
+    pokemonList.forEach(pokemon => {
+        const pokemonCard = document.createElement('div');
+        pokemonCard.classList.add('search-champion'); 
+
+        const imageDiv = document.createElement('div');
+        imageDiv.classList.add('search-champion-img'); 
+
+        if (pokemon.image) {
+            const image = document.createElement('img');
+            image.src = pokemon.image;
+            imageDiv.appendChild(image);
+        }
+
+
+        const removFromTeamBtn = document.createElement('button');
+        removFromTeamBtn.classList.add('add-to-team-btn');
+        removFromTeamBtn.textContent = 'remove from team';
+
+        removFromTeamBtn.addEventListener('click', function() {
+            
+            
+
+            // Uppdatera listorna efter att ha lagt till i team
+            updateLists();
+        });
+
+        const infoDiv = document.createElement('div');
+        infoDiv.classList.add('infoDiv'); 
+        const nameHeader = document.createElement('h2');
+        nameHeader.textContent = pokemon.name;
+
+        infoDiv.appendChild(nameHeader);
+        infoDiv.appendChild(removFromTeamBtn);
+        pokemonCard.appendChild(imageDiv);
+        pokemonCard.appendChild(infoDiv);
+
+        list.appendChild(pokemonCard);
+    });
+}
+
+function updateLists() {
+    const valdaPokemonsList = document.getElementById('valdaPokemonsList');
+    const reservPokemonsList = document.getElementById('reservPokemonsList');
+
+    // Återställ listorna
+    valdaPokemonsList.innerHTML = '';
+    reservPokemonsList.innerHTML = '';
+
+
+    createPokemonListElement('valdaPokemonsList', valdaPokemons);
+
+
+    createPokemonListElement('reservPokemonsList', reservPokemons);
+}
 
 
 
